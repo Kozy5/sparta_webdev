fec14cb81d18d729a47d1bdb18d58aba190504bd
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-03-25 03:19:54 +0900

    Kotlin prac init content

--- kotlin/prac.kt
+++ kotlin/prac.kt
@@ -1,25 +1,5 @@
 fun main() {
-    class player(var nickname:String,var skill:String, var level:Int){
-    fun levelUp(){
-        println("${nickname}소환사님 레벨업! ${level} 레벨이 되신것을 축하합니다!")
-        level++
-    } 
-    fun recomendSkill(){
-        println("${skill}스킬을 써보시는것도 괜찮을꺼같아요")
-    }
-    fun useSkill(){
-        println("용맹한 ${nickname}의 ${skill}!!!!!!!!!!!")
-    }
-}
-    var nickName = "구맹"
-    var skillName = "몰라의 일격"
-    var lep = 1
-    var sohwan = player(nickName,skillName,lep)
-    
-    
-    sohwan.levelUp()
-   
-    
-    
-}
-
+    var list = listOf(1,2,3,4,5)
+    print(list.indices.forEach{it+1})
+ 
+ }
