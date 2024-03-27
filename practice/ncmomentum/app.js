01edb42cc6c17f14c2397810b4b63d8cd8f1842d
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-03-27 23:48:51 +0900

    ncMomentum practice about js
    addEventListner

--- practice/ncmomentum/app.js
+++ practice/ncmomentum/app.js
@@ -1,1 +1,8 @@
-alert("hi");
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function click(){
+    console.log("왜 클릭을 했을까")
+}
+addEventListener("click",click)
