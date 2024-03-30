17d0ecc401bdd8e322b2554df82fdc934207e025
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-03-30 16:57:49 +0900

    거지 키우기 CSS Update 1.1
    1. 파워업 버튼, 고용하기 버튼 클래스 나누고 자바스크립트에 변수 새로 저장
    2. 자동수금, 구걸하기 별도로 기능 저장

--- practice/poor/js/app.js
+++ practice/poor/js/app.js
@@ -1,30 +1,55 @@
 const mainbutton = document.querySelector("#main-button")
-const goyongbutton = document.querySelectorAll("#store-detail-list > div > div > button")
+const goyongbutton = document.querySelectorAll(".partner-button")
+const powerbutton = document.querySelectorAll(".powerup-button")
 const money = document.querySelector("#money-box h5")
 setInterval(gugul,1000)
-
+var moneyauto = 1
+var moneycliced = 10
 var moneyDefault = 0
 var intervalValue = 1000
 
+
 function gugul(){
     
-    moneyDefault += 1
+    moneyDefault += moneyauto
     money.innerText = moneyDefault;
 }
 
-function gumae(event){
+function directGugul(){
+    moneyDefault += moneycliced
+    money.innerText=moneyDefault;
+}
+function buyPowerUpAuto(event){
     event.preventDefault();
     var gumaemoney = parseInt(event.target.innerHTML)
     var chodang = parseInt(event.target.parentElement.id)
     if(moneyDefault >= gumaemoney){
         moneyDefault -= gumaemoney
     money.innerText = moneyDefault;
-    setInterval(gugul,chodang)
+    moneyauto = chodang
     }else{
         alert("돈이 부족하네요")
     }
 }
-mainbutton.addEventListener("click",gugul);
+
+function buyPowerUpClick(event){
+    event.preventDefault();
+    var gumaemoney = parseInt(event.target.innerHTML)
+    var powervalue = parseInt(event.target.parentElement.id)
+    if(moneyDefault >= gumaemoney){
+        moneyDefault -= gumaemoney
+    money.innerText = moneyDefault;
+    moneycliced = powervalue
+    }else{
+        alert("돈이 부족하네요")
+    }
+}
+
+mainbutton.addEventListener("click",directGugul);
 goyongbutton.forEach.call(goyongbutton,function(col){ 
-	col.addEventListener("click",gumae); 
+	col.addEventListener("click",buyPowerUpAuto); 
 }); 
+powerbutton.forEach.call(powerbutton,function(col){ 
+	col.addEventListener("click",buyPowerUpClick); 
+}); 
+
