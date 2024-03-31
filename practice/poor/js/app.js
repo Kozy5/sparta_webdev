31df4cb519d0753b747e475bf44aa10f1300be1a
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-03-31 16:10:13 +0900

    거지키우기 JS Update
    1. 버튼을 통해 구매시 해당 버튼을 담은 div 사라지게하는 기능 추가

--- practice/poor/js/app.js
+++ practice/poor/js/app.js
@@ -1,55 +1,54 @@
-const mainbutton = document.querySelector("#main-button")
-const goyongbutton = document.querySelectorAll(".partner-button")
-const powerbutton = document.querySelectorAll(".powerup-button")
-const money = document.querySelector("#money-box h5")
-setInterval(gugul,1000)
-var moneyauto = 1
-var moneycliced = 10
-var moneyDefault = 0
-var intervalValue = 1000
+const mainbutton = document.querySelector("#main-button");
+const goyongbutton = document.querySelectorAll(".partner-button");
+const powerbutton = document.querySelectorAll(".powerup-button");
+const money = document.querySelector("#money-box h5");
+setInterval(gugul, 1000);
+var moneyauto = 1;
+var moneycliced = 10;
+var moneyDefault = 0;
+var intervalValue = 1000;
 
+function gugul() {
+  moneyDefault += moneyauto;
+  money.innerText = moneyDefault;
+}
 
-function gugul(){
-    
-    moneyDefault += moneyauto
+function directGugul() {
+  moneyDefault += moneycliced;
+  money.innerText = moneyDefault;
+}
+function buyPowerUpAuto(event) {
+  event.preventDefault();
+  var gumaemoney = parseInt(event.target.innerHTML);
+  var chodang = parseInt(event.target.parentElement.id);
+  if (moneyDefault >= gumaemoney) {
+    moneyDefault -= gumaemoney;
     money.innerText = moneyDefault;
+    moneyauto = chodang;
+    event.target.parentElement.parentElement.classList.add("hidden");
+  } else {
+    alert("돈이 부족하네요");
+  }
 }
 
-function directGugul(){
-    moneyDefault += moneycliced
-    money.innerText=moneyDefault;
-}
-function buyPowerUpAuto(event){
-    event.preventDefault();
-    var gumaemoney = parseInt(event.target.innerHTML)
-    var chodang = parseInt(event.target.parentElement.id)
-    if(moneyDefault >= gumaemoney){
-        moneyDefault -= gumaemoney
+function buyPowerUpClick(event) {
+  event.preventDefault();
+  var gumaemoney = parseInt(event.target.innerHTML);
+  var powervalue = parseInt(event.target.parentElement.id);
+  if (moneyDefault >= gumaemoney) {
+    moneyDefault -= gumaemoney;
     money.innerText = moneyDefault;
-    moneyauto = chodang
-    }else{
-        alert("돈이 부족하네요")
-    }
+    moneycliced = powervalue;
+    event.target.parentElement.parentElement.classList.add("hidden");
+  } else {
+    alert("돈이 부족하네요");
+  }
 }
 
-function buyPowerUpClick(event){
-    event.preventDefault();
-    var gumaemoney = parseInt(event.target.innerHTML)
-    var powervalue = parseInt(event.target.parentElement.id)
-    if(moneyDefault >= gumaemoney){
-        moneyDefault -= gumaemoney
-    money.innerText = moneyDefault;
-    moneycliced = powervalue
-    }else{
-        alert("돈이 부족하네요")
-    }
-}
-
-mainbutton.addEventListener("click",directGugul);
-goyongbutton.forEach.call(goyongbutton,function(col){ 
-	col.addEventListener("click",buyPowerUpAuto); 
-}); 
-powerbutton.forEach.call(powerbutton,function(col){ 
-	col.addEventListener("click",buyPowerUpClick); 
-}); 
-
+mainbutton.addEventListener("click", directGugul);
+goyongbutton.forEach.call(goyongbutton, function (col) {
+  col.addEventListener("click", buyPowerUpAuto);
+});
+powerbutton.forEach.call(powerbutton, function (col) {
+  col.addEventListener("click", buyPowerUpClick);
+});
