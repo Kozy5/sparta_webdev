6902ba067dac9a0dd6f96f0ba888d3a6c9ccb815
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-04-03 22:09:09 +0900

    이든 해저드 키우기 JS Update
    1. 변수명 수정

--- practice/poor/js/app.js
+++ practice/poor/js/app.js
@@ -1,60 +1,71 @@
-const mainbutton = document.querySelector("#main-button");
-const goyongbutton = document.querySelectorAll(".partner-button");
-const powerbutton = document.querySelectorAll(".powerup-button");
+const mainbtn = document.querySelector("#main-button");
+const hragentbtn = document.querySelectorAll(".partner-button");
+const plusmenubtn = document.querySelectorAll(".powerup-button");
 const money = document.querySelector("#money-box h5");
 const partnerbox = document.querySelector("#partner-box");
-setInterval(gugul, 1000);
+const dangermsg = document.querySelector(".danger-message");
+
+setInterval(begging, 1000);
+
 var moneyauto = 1;
 var moneycliced = 1;
 var moneyDefault = 0;
 var intervalValue = 1000;
 
-function gugul() {
+function begging() {
   moneyDefault += moneyauto;
   money.innerText = moneyDefault;
 }
 
-function directGugul() {
+function directBegging() {
   moneyDefault += moneycliced;
   money.innerText = moneyDefault;
 }
-function buyPowerUpAuto(event) {
+
+function addClassHidden() {
+  dangermsg.classList.add("hidden")
+}
+function plusHrAgent(event) {
   event.preventDefault();
-  var gumaemoney = parseInt(event.target.id);
-  var chodang = parseInt(event.target.parentElement.id);
-  var partenername = event.target.parentElement.parentElement.firstElementChild.innerHTML;
-  if (moneyDefault >= gumaemoney) {
-    moneyDefault -= gumaemoney;
+  var buymoney = parseInt(event.target.id);
+  var plusAutoMoney = parseInt(event.target.parentElement.id);
+  var ptnname = event.target.parentElement.parentElement.firstElementChild.innerHTML;
+  if (moneyDefault >= buymoney) {
+    moneyDefault -= buymoney;
     money.innerText = moneyDefault;
-    moneyauto = chodang;
+    moneyauto += plusAutoMoney;
     var temp = document.createElement("h4")
-    temp.innerHTML = partenername
+    temp.innerHTML = ptnname
     temp.classList.add("addparteners")
     partnerbox.append(temp)
     event.target.parentElement.parentElement.classList.add("hidden");
   } else {
-    alert("돈이 부족하네요");
+    dangermsg.classList.remove("hidden");
+    setTimeout(addClassHidden,2000);
   }
 }
 
-function buyPowerUpClick(event) {
+function plusMenu(event) {
   event.preventDefault();
-  var gumaemoney = parseInt(event.target.id);
-  var powervalue = parseInt(event.target.parentElement.id);
-  if (moneyDefault >= gumaemoney) {
-    moneyDefault -= gumaemoney;
+  var buymoney = parseInt(event.target.id);
+  var plusmenuvalue = parseInt(event.target.parentElement.id);
+  if (moneyDefault >= buymoney) {
+    moneyDefault -= buymoney;
     money.innerText = moneyDefault;
-    moneycliced = powervalue;
+    moneycliced += plusmenuvalue;
     event.target.parentElement.parentElement.classList.add("hidden");
   } else {
-    alert("돈이 부족하네요");
+    dangermsg.classList.remove("hidden");
+    setTimeout(addClassHidden,2000);
   }
 }
 
-mainbutton.addEventListener("click", directGugul);
-goyongbutton.forEach.call(goyongbutton, function (col) {
-  col.addEventListener("click", buyPowerUpAuto);
+mainbtn.addEventListener("click", directBegging);
+
+hragentbtn.forEach.call(hragentbtn, function (col) {
+  col.addEventListener("click", plusHrAgent);
 });
-powerbutton.forEach.call(powerbutton, function (col) {
-  col.addEventListener("click", buyPowerUpClick);
+
+plusmenubtn.forEach.call(plusmenubtn, function (col) {
+  col.addEventListener("click", plusMenu);
 });
