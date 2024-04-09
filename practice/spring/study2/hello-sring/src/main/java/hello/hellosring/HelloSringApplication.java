aa96ff23808c600625e8eb0d8400a60794a77059
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-04-09 18:34:56 +0900

    Spring(Java) Study - basic
    1. 초반단계 Spring init

---
+++ practice/spring/study2/hello-sring/src/main/java/hello/hellosring/HelloSringApplication.java
@@ -0,0 +1,13 @@
+package hello.hellosring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSringApplication.class, args);
+	}
+
+}
---
+++ practice/spring/study2/hello-sring/src/test/java/hello/hellosring/HelloSringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellosring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
