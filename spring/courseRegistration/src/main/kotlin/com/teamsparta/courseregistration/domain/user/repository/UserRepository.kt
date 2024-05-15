d4f99fb91626bd69609f87b4b255dccc4ed893df
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-05-15 11:30:12 +0900

    🚚 chore : Spring&Kotlin 숙련 1-20
    JpaRepository 파일 생성

---
+++ spring/courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/user/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.teamsparta.courseregistration.domain.user.repository
+
+import com.teamsparta.courseregistration.domain.user.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository: JpaRepository<User,Long> {
+}
