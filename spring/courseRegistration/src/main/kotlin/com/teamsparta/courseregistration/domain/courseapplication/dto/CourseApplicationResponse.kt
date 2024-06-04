c6a98edfa524ef31d69b4f98a452d74ca65a8379
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-06-04 23:36:49 +0900

    ✨ feat : CourseApplication 기능 구현을 위한 코드 추가 및 수정

--- spring/courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/courseapplication/dto/CourseApplicationResponse.kt
+++ spring/courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/courseapplication/dto/CourseApplicationResponse.kt
@@ -1,10 +1,14 @@
 package com.teamsparta.courseregistration.domain.courseapplication.dto
 
 import com.teamsparta.courseregistration.domain.course.dto.CourseResponse
+import com.teamsparta.courseregistration.domain.course.model.Course
+import com.teamsparta.courseregistration.domain.courseapplication.model.CourseApplicationStatus
 import com.teamsparta.courseregistration.domain.user.dto.UserResponse
+import com.teamsparta.courseregistration.domain.user.model.User
 
 data class CourseApplicationResponse(
     val id:Long,
-    val course:CourseResponse,
-    val user:UserResponse
+    val course: CourseResponse,
+    val user: UserResponse,
+    val status: String
 )
