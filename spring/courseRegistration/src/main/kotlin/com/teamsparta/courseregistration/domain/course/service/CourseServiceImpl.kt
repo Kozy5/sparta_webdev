e7a1688cb7a9b7316c305297b647acafea8b51e8
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-06-04 23:35:35 +0900

    ✨ feat :  Course Model 수정, Course Service 기능들 구현
    미 기능 구현 대부분 구현

--- spring/courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/course/service/CourseServiceImpl.kt
+++ spring/courseRegistration/src/main/kotlin/com/teamsparta/courseregistration/domain/course/service/CourseServiceImpl.kt
@@ -4,23 +4,38 @@ import com.teamsparta.courseregistration.domain.course.dto.CourseResponse
 import com.teamsparta.courseregistration.domain.course.dto.CreateCourseRequest
 import com.teamsparta.courseregistration.domain.course.dto.UpdateCourseRequest
 import com.teamsparta.courseregistration.domain.course.model.Course
+import com.teamsparta.courseregistration.domain.course.model.CourseStatus
 import com.teamsparta.courseregistration.domain.course.model.toResponse
 import com.teamsparta.courseregistration.domain.course.repository.CourseRepository
 import com.teamsparta.courseregistration.domain.courseapplication.dto.ApplyCourseRequest
 import com.teamsparta.courseregistration.domain.courseapplication.dto.CourseApplicationResponse
 import com.teamsparta.courseregistration.domain.courseapplication.dto.UpdateApplicationStatusRequest
+import com.teamsparta.courseregistration.domain.courseapplication.model.CourseApplication
+import com.teamsparta.courseregistration.domain.courseapplication.model.CourseApplicationStatus
+import com.teamsparta.courseregistration.domain.courseapplication.model.toResponse
+import com.teamsparta.courseregistration.domain.courseapplication.repository.CourseApplicationRepository
 import com.teamsparta.courseregistration.domain.exception.ModelNotFoundException
 import com.teamsparta.courseregistration.domain.lecture.dto.AddLectureRequest
 import com.teamsparta.courseregistration.domain.lecture.dto.LectureResponse
 import com.teamsparta.courseregistration.domain.lecture.dto.UpdateLectureRequest
+import com.teamsparta.courseregistration.domain.lecture.model.Lecture
+import com.teamsparta.courseregistration.domain.lecture.model.toResponse
+import com.teamsparta.courseregistration.domain.lecture.repository.LectureRepository
+import com.teamsparta.courseregistration.domain.user.repository.UserRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CourseServiceImpl(
-    val courseRepository: CourseRepository
+    private val courseRepository: CourseRepository,
+    private val lectureRepository: LectureRepository,
+    private val userRepository: UserRepository,
+    private val courseApplicationRepository: CourseApplicationRepository,
+    applicationRepository: CourseApplicationRepository
 ):CourseService {
+
+
     override fun getAllCourseList(): List<CourseResponse> {
         return courseRepository.findAll().map {it.toResponse()}
     }
@@ -52,59 +67,69 @@ class CourseServiceImpl(
     }
 
     override fun getLectureList(courseId: Long): List<LectureResponse> {
-        // TODO: 만약 courseId에 해당하는 Course가 없다면 throw ModelNotFoundException
-        // TODO: DB에서 courseId에 해당하는 Course목록을 가져오고, 하위 lecture들을 가져온 다음, LectureResopnse로 변환해서 반환
-        TODO("Not yet implemented")
+        val course = courseRepository.findByIdOrNull(courseId) ?: throw ModelNotFoundException("course",courseId)
+        return course.lectures.map{it.toResponse()}
     }
 
     override fun getLecture(courseId: Long, lectureId: Long): LectureResponse {
-        // TODO: 만약 courseId, lectureId에 해당하는 Lecture가 없다면 throw ModelNotFoundException
-        // TODO: DB에서 courseId, lectureId에 해당하는 Lecture를 가져와서 LectureResponse로 변환 후 반환
-        TODO("Not yet implemented")
+        val lecture = lectureRepository.findByCourseIdAndId(courseId, lectureId) ?: throw ModelNotFoundException("lecture",lectureId)
+        return lecture.toResponse()
     }
 
     @Transactional
     override fun addLecture(courseId: Long, request: AddLectureRequest): LectureResponse {
-        // TODO: 만약 courseId에 해당하는 Course가 없다면 throw ModelNotFoundException
-        // TODO: DB에서 courseId에 해당하는 Course를 가져와서 Lecture를 추가 후 DB에 저장, 결과를을 LectureResponse로 변환 후 반환
-        TODO("Not yet implemented")
+        val course = courseRepository.findByIdOrNull(courseId) ?: throw ModelNotFoundException("course", courseId)
+        val lecture = Lecture(
+            title = request.title,
+            videoUrl = request.videoUrl,
+            course = course
+        )
+        return lectureRepository.save(lecture).toResponse()
     }
 
     @Transactional
     override fun updateLecture(courseId: Long, lectureId: Long, request: UpdateLectureRequest): LectureResponse {
-        // TODO: 만약 courseId, lectureId에 해당하는 Lecture가 없다면 throw ModelNotFoundException
-        /* TODO: DB에서 courseId, lectureId에 해당하는 Lecture를 가져와서
-            request로 업데이트 후 DB에 저장, 결과를을 LectureResponse로 변환 후 반환 */
-        TODO("Not yet implemented")
+        val lecture = lectureRepository.findByCourseIdAndId(courseId,lectureId) ?: throw ModelNotFoundException("lecture",lectureId)
+        lecture.title = request.title
+        lecture.videoUrl = request.videoUrl
+        return lectureRepository.save(lecture).toResponse()
     }
 
     @Transactional
     override fun removeLecture(courseId: Long, lectureId: Long) {
-        // TODO: 만약 courseId에 해당하는 Course가 없다면 throw ModelNotFoundException
-        // TODO: DB에서 courseId, lectureId에 해당하는 Lecture를 가져오고, 삭제
-        TODO("Not yet implemented")
+        val course = courseRepository.findByIdOrNull(courseId) ?: throw ModelNotFoundException("course",courseId)
+        val lecture = lectureRepository.findByCourseIdAndId(courseId,lectureId) ?: throw ModelNotFoundException("lecture",lectureId)
+        course.lectures.remove(lecture)
+        courseRepository.save(course)
     }
 
     @Transactional
     override fun applyCourse(courseId: Long, request: ApplyCourseRequest): CourseApplicationResponse {
-        // TODO: 만약 courseId에 해당하는 Course가 없다면 throw ModelNotFoundException
-        // TODO: 만약 course가 이미 마감됐다면, throw IllegalStateException
-        // TODO: 이미 신청했다면, throw  IllegalStateException
+        val course = courseRepository.findByIdOrNull(courseId) ?: throw ModelNotFoundException("course",courseId)
+        val user = userRepository.findByIdOrNull(request.userId) ?: throw ModelNotFoundException("user",request.userId)
 
-        TODO("Not yet implemented")
+        if(course.status == CourseStatus.CLOSED){
+            throw IllegalStateException("Course is already closed. courseId: $courseId")
+        }else if(courseApplicationRepository.existsByCourseIdAndUserId(courseId, request.userId)){
+            throw IllegalStateException("Already applied. courseId: $courseId, userId: $request.userId")
+        }
+
+        val courseApplication = CourseApplication(
+            course = course,
+            user = user
+        )
+
+        return courseApplicationRepository.save(courseApplication).toResponse()
     }
 
     override fun getCourseApplication(courseId: Long, applicationId: Long): CourseApplicationResponse {
-        // TODO: 만약 courseId, applicationId에 해당하는 CourseApplication이 없다면 throw ModelNotFoundException
-        // TODO: DB에서 courseId, applicationId에 해당하는 CourseApplication을 가져와서 CourseApplicationResponse로 변환 후 반환
-        TODO("Not yet implemented")
+        val courseApplication = courseApplicationRepository.findByCourseIdAndId(courseId, applicationId) ?: throw ModelNotFoundException("courseApplication",applicationId)
+        return courseApplication.toResponse()
     }
 
     override fun getCourseApplicationList(courseId: Long): List<CourseApplicationResponse> {
-        // TODO: 만약 courseId에 해당하는 Course가 없다면 throw ModelNotFoundException
-        // TODO: DB에서 courseId에 해당하는 Course를 가져오고, 하위 courseApplication들을 CourseApplicationResponse로 변환 후 반환
-        TODO("Not yet implemented")
-
+        val course = courseRepository.findByIdOrNull(courseId) ?: throw ModelNotFoundException("course", courseId)
+        return course.courseApplications.map{it.toResponse()}
     }
 
     @Transactional
@@ -113,12 +138,30 @@ class CourseServiceImpl(
         applicationId: Long,
         request: UpdateApplicationStatusRequest
     ): CourseApplicationResponse {
-        // TODO: 만약 courseId, applicationId에 해당하는 CourseApplication이 없다면 throw ModelNotFoundException
-        // TODO: 만약 status가 이미 변경된 상태면 throw IllegalStateException
-        // TODO: Course의 status가 CLOSED상태 일시 throw IllegalStateException
-        // TODO: 승인을 하는 케이스일 경우, course의 numApplicants와 maxApplicants가 동일하면, course의 상태를 CLOSED로 변경
-        // TODO: DB에서 courseApplication을 가져오고, status를 request로 업데이트 후 DB에 저장, 결과를 CourseApplicationResponse로 변환 후 반환
-        TODO("Not yet implemented")
+        val course = courseRepository.findByIdOrNull(courseId) ?: throw ModelNotFoundException("course",courseId)
+        val courseApplication = courseApplicationRepository.findByCourseIdAndId(courseId, applicationId) ?: throw ModelNotFoundException("courseApplication",applicationId)
+        // 이미 처리 완료된 건인지 체크
+        if(courseApplication.status != CourseApplicationStatus.PENDING){
+            throw IllegalStateException("Already Completed CourseId : $courseId ApplicationId : $applicationId")
+        }else if(course.status == CourseStatus.CLOSED){
+            throw IllegalStateException("Already Closed Course CourseId : $courseId")
+        }
+        // 승인 / 거절에 따른 처리
+        if(request.status == CourseApplicationStatus.ACCEPTED.name){
+            courseApplication.status = CourseApplicationStatus.ACCEPTED
+            course.numApplication+=1
+            if(course.numApplication >= course.maxApplication){
+                course.status = CourseStatus.CLOSED
+            }
+            courseRepository.save(course)
+        }else if(request.status == CourseApplicationStatus.REJECTED.name){
+            courseApplication.status = CourseApplicationStatus.REJECTED
+        }else{
+            throw IllegalArgumentException("Invalid status : ${request.status}")
+        }
+
+        return courseApplicationRepository.save(courseApplication).toResponse()
+
     }
 
 }
